021c9269daeff0d73fae8c989dcaf1bc01d7e32f
Bharat Bhusal <bharat@Bharats-MacBook-Air.local>
2023-06-12 12:30:27 +0530

    first_commit

---
+++ default/target/debug/deps/default-a57ce69176af1752.d
@@ -0,0 +1,5 @@
+/Users/bharat/Documents/learn_rust/default/target/debug/deps/default-a57ce69176af1752.rmeta: src/main.rs
+
+/Users/bharat/Documents/learn_rust/default/target/debug/deps/default-a57ce69176af1752.d: src/main.rs
+
+src/main.rs:
---
+++ default/target/debug/deps/default-f3ec2df66647ac75.d
@@ -0,0 +1,5 @@
+/Users/bharat/Documents/learn_rust/default/target/debug/deps/default-f3ec2df66647ac75.rmeta: src/main.rs
+
+/Users/bharat/Documents/learn_rust/default/target/debug/deps/default-f3ec2df66647ac75.d: src/main.rs
+
+src/main.rs:
