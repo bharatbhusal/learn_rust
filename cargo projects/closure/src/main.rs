7874997794803cfc3c5b414f4678f08ff97bc6b3
Bharat Bhusal <bharat@Bharats-MacBook-Air.local>
2023-06-16 15:25:09 +0530

    closure basics

--- cargo projects/closure/src/main.rs
+++ cargo projects/closure/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    // Immutable closure
+    let name1 = "Bharat";
+    println!("Name before closure: {}", name1);
+    let immutable_closure = |x| println!("Hi {}", x);
+    immutable_closure(name1);
+    println!("Name after closure: {}\n", name1);
+
+    //Mutable closure
+    let mut name2 = "Bharat".to_string();
+    println!("Name before closure: {}", name2);
+    let immutable_closure = |x: &mut String| {
+        *x = "Geeta".to_string();
+        println!("Hi {}", x)
+    };
+    immutable_closure(&mut name2);
+    println!("Name after closure: {}\n", name2);
+
+    //Owned closure
+    let name2 = "Bharat".to_string();
+    println!("Name before closure: {}", name2);
+    let immutable_closure = |mut x: String| {
+        x = "Geeta".to_string();
+        println!("Hi {}", x)
+    };
+    immutable_closure(name2);
+    println!(
+        "Name after closure: {}\n", /*name2 */
+        "cannot display name"
+    );
 }
