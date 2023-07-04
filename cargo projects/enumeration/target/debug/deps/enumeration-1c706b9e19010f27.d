a6fecc1c5e96502e5d7b4a21fbfb6d4c4ece0cc9
Bharat Bhusal <bharat@Bharats-MacBook-Air.local>
2023-07-04 21:07:29 +0530

    Trial

---
+++ cargo projects/enumeration/target/debug/deps/enumeration-1c706b9e19010f27.d
@@ -0,0 +1,5 @@
+/Users/bharat/Desktop/learn_rust/cargo projects/enumeration/target/debug/deps/enumeration-1c706b9e19010f27: src/main.rs
+
+/Users/bharat/Desktop/learn_rust/cargo projects/enumeration/target/debug/deps/enumeration-1c706b9e19010f27.d: src/main.rs
+
+src/main.rs:
---
+++ cargo projects/enumeration/target/debug/deps/enumeration-6632eb528939461e.d
@@ -0,0 +1,11 @@
+/Users/bharat/Desktop/learn_rust/cargo projects/enumeration/target/debug/deps/enumeration-6632eb528939461e.rmeta: src/lib.rs src/advanced.rs src/basic.rs src/if_let.rs src/other_keyword.rs
+
+/Users/bharat/Desktop/learn_rust/cargo projects/enumeration/target/debug/deps/libenumeration-6632eb528939461e.rlib: src/lib.rs src/advanced.rs src/basic.rs src/if_let.rs src/other_keyword.rs
+
+/Users/bharat/Desktop/learn_rust/cargo projects/enumeration/target/debug/deps/enumeration-6632eb528939461e.d: src/lib.rs src/advanced.rs src/basic.rs src/if_let.rs src/other_keyword.rs
+
+src/lib.rs:
+src/advanced.rs:
+src/basic.rs:
+src/if_let.rs:
+src/other_keyword.rs:
